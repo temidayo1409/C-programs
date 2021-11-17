292875f494085aec160dc6a8112ae7a1033d55ae
temidayo1409 <75425804+temidayo1409@users.noreply.github.com>
2021-11-17 05:52:14 -0500

    Update Random Race.cpp

--- Random Race.cpp
+++ Random Race.cpp
@@ -6,9 +6,11 @@ using namespace std;
 
 int main(){
   int runner1 = 0, runner2 = 0, finishline = 100, random2;
+  cout << "The race has begun!" << endl;
+  cout << endl;
+  srand(time(0));
   while (runner1 < finishline && runner2 < finishline){
     runner1 += 2;
-    srand(time(0));
     random2 = 1 + rand() % 12;
     if (runner2 % 2 == 0) {
       runner2 += random2;
@@ -19,6 +21,10 @@ int main(){
         runner2 = 0;
       }
     }
+    cout << "Right now, Runner1 has covered " << runner1 << " steps";
+    cout << endl;
+    cout << "While Runner2 has covered " << runner2 << " steps!";
+    cout << endl << endl;
   }
 
   if (runner1 >= finishline && runner2 >= finishline){
